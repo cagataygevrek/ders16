faab926ecfb399d0386fe61bd45f03df15f8ef1e
Cagatay Gevrek <cagataygevrek@gmail.com>
2023-01-01 18:01:05 +0300

    Redux uygulaması öncesi statik kodlar yazıldı

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoEklemeFormu from "./components/TodoEklemeFormu";
+import TodoListesi from "./components/TodoListesi";
+import TodoTamamlananSayisi from "./components/TodoTamamlananSayisi";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className='container bg-white p-4 mt-5'>
+      <h1>Yapılacaklar Listem</h1>
+      <TodoEklemeFormu />
+      <TodoListesi />
+      <TodoTamamlananSayisi />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TodoTamamlananSayisi.jsx
@@ -0,0 +1,5 @@
+const TodoTamamlananSayisi = () => {
+  return <h4 className='mt-3'>Tamamlanan İş Sayısı: 5</h4>;
+};
+
+export default TodoTamamlananSayisi;
